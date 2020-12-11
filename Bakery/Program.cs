55cc198f760d684fdcb8b12b31c75e28cb571fe9
Constantine <faustlarsen@gmail.com>
2020-12-11 16:32:57 -0700

    add UI logic to return to for pastries and entire order

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,12 +8,15 @@ namespace PierresBakery
     public static void Main()
     {
       Console.WriteLine("Welcome to the Pierre's Bakery!");
-      Console.WriteLine("Would you like to buy some bread?");
-      Console.WriteLine("How many? 1 for $5 or buy 2 get 1 free?");
-      Bread purchase = new Bread(int.Parse(Console.ReadLine()));
-      int breadAmount = purchase.BreadTotal();
-      Console.WriteLine(breadAmount);
-      
+      Console.WriteLine("Would you like to buy some Bread and Pastry?");
+      Console.WriteLine("BREAD: 1 for $5 or buy 2 get 1 FREE! PASTRY: 1 for $2 or 3 for $5!");
+      Console.WriteLine("How many loaves of bread?");
+      Bread purchaseBread = new Bread(int.Parse(Console.ReadLine()));
+      int breadAmount = purchaseBread.BreadTotal();
+      Console.WriteLine("And how many pastries?");
+      Pastry purchasePastry = new Pastry(int.Parse(Console.ReadLine()));
+      int pastryAmount = purchasePastry.PastryTotal();
+      Console.WriteLine(breadAmount + pastryAmount);
     }
   }
 }
