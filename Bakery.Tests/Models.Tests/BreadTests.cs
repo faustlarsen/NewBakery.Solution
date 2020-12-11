c354191c754092a362191862273612fca8e62f9a
Constantine <faustlarsen@gmail.com>
2020-12-11 13:31:21 -0700

    create bread class and pass constructor test

--- Bakery.Tests/Models.Tests/BreadTests.cs
+++ Bakery.Tests/Models.Tests/BreadTests.cs
@@ -6,9 +6,11 @@ namespace Bakery.Tests
   [TestClass]
   public class TestBread
   {
-
-
-    
-
+    [TestMethod]
+    public void BreadConstructor_InstanceOfBread_True()
+    {
+    Bread purchase = new Bread(2);
+    Assert.AreEqual(typeof(Bread), purchase.GetType());
+    }
   }
 } 
