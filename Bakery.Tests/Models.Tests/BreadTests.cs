08ff40dcd65a060089f200fb0c1be9ca23766ef5
Constantine <faustlarsen@gmail.com>
2020-12-11 16:17:39 -0700

    create and fail test to return total for pastry

--- Bakery.Tests/Models.Tests/BreadTests.cs
+++ Bakery.Tests/Models.Tests/BreadTests.cs
@@ -4,40 +4,36 @@ using Bakery.Models;
 
 namespace Bakery.Tests
 {
-  [TestClass]
-  public class TestBread
-  {
-    [TestMethod]
-    public void BreadConstructor_InstanceOfBread_True()
+    [TestClass]
+    public class TestBread
     {
-    Bread purchase = new Bread(5);
-    Assert.AreEqual(typeof(Bread), purchase.GetType());
-    }
-    [TestMethod]
-    public void BreadConstructor_AcceptsAmount_Int()
-    {
-    Bread purchase = new Bread(5);
-    Assert.AreEqual(true, purchase.AmountOfBread());
-    }
-    [TestMethod]
-    public void BreadPrice_AssignPriceToBread_Int()
-    {
-      Bread purchase = new Bread(5);
-      Dictionary<string, int> value = new Dictionary<string, int> 
-      {
-        {"Bread", 5}
-      };
-      CollectionAssert.AreEqual(value, purchase.BreadPrice());
-    }
-    [TestMethod]
-    public void BreadCost_ReturnCostOfBread_Int()
-    {
-      Bread newOrder = new Bread(5);
-      Dictionary<string, int> value = new Dictionary<string, int>
-      {
-        {"Bread", 5}
-      };
-      Assert.AreEqual(25, newOrder.BreadPrice(value));
-    }
-  } 
+        [TestMethod]
+            public void BreadConstructor_InstanceOfBread_True()
+        {
+            Bread purchase = new Bread(5);
+            Assert.AreEqual(typeof(Bread), purchase.GetType());
+        }
+            [TestMethod]
+            public void BreadConstructor_AcceptsAmount_Int()
+        {
+            Bread purchase = new Bread(5);
+            Assert.AreEqual(true, purchase.AmountOfBread());
+        }
+            [TestMethod]
+            public void BreadPrice_AssignPriceToBread_Int()
+        {
+            Bread purchase = new Bread(5);
+            Dictionary<string, int> value = new Dictionary<string, int> 
+            {
+              {"Bread", 5}
+            };
+            CollectionAssert.AreEqual(value, purchase.BreadPrice());
+        }
+            [TestMethod]
+            public void BreadTotal_ReturnCostOfBread_Int()
+        {
+            Bread newOrder = new Bread(5);
+            Assert.AreEqual(25, newOrder.BreadTotal());
+        }
+    } 
 }
--- Bakery.Tests/Models.Tests/PastryTests.cs
+++ Bakery.Tests/Models.Tests/PastryTests.cs
@@ -4,30 +4,36 @@ using Bakery.Models;
 
 namespace Bakery.Tests
 {
-  [TestClass]
-  public class TestPastry
-  {
-    [TestMethod]
-    public void PastryConstructor_InstanceOfPastry_True()
+    [TestClass]
+    public class TestPastry
     {
-    Pastry purchase = new Pastry(2);
-    Assert.AreEqual(typeof(Pastry), purchase.GetType());
+        [TestMethod]
+        public void PastryConstructor_InstanceOfPastry_True()
+        {
+            Pastry purchase = new Pastry(2);
+            Assert.AreEqual(typeof(Pastry), purchase.GetType());
+        }
+            [TestMethod]
+            public void PastryConstructor_AcceptsAmount_Int()
+        {
+            Pastry purchase = new Pastry(2);
+            Assert.AreEqual(true, purchase.AmountOfPastry());
+        }
+            [TestMethod]
+            public void PastryPrice_AssignCostToPastry_Int()
+        {
+            Pastry purchase = new Pastry(2);
+            Dictionary<string, int> value = new Dictionary<string, int> 
+            {
+              {"Pastry", 2}
+            };
+            CollectionAssert.AreEqual(value, purchase.PastryPrice());
+        }
+            [TestMethod]
+            public void PastryTotal_ReturnCostOfPastry_Int()
+        {
+            Pastry newOrder = new Pastry(2);
+            Assert.AreEqual(4, newOrder.PastryTotal());
+        }
     }
-    [TestMethod]
-    public void PastryConstructor_AcceptsAmount_Int()
-    {
-    Pastry purchase = new Pastry(2);
-    Assert.AreEqual(true, purchase.AmountOfPastry());
-    }
-    [TestMethod]
-    public void PastryPrice_AssignCostToPastry_Int()
-    {
-      Pastry purchase = new Pastry(2);
-      Dictionary<string, int> value = new Dictionary<string, int> 
-      {
-        {"Pastry", 2}
-      };
-      CollectionAssert.AreEqual(value, purchase.PastryPrice());
-    }
-  }
 }
