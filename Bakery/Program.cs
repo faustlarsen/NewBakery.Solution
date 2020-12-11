54b97aac2c7e99dd57a79e1720789540014b6c92
Constantine <faustlarsen@gmail.com>
2020-12-11 15:41:37 -0700

    initialize user interface logic

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,10 +1,16 @@
+using System;
+using Bakery.Models;
+
 namespace PierresBakery
 {
   public class Bakery
   {
     public static void Main()
     {
-
+    Console.WriteLine("Welcome to the Pierre's Bakery!");
+    Console.WriteLine("Would you like to buy bread or pastry?");
+    string respond = Console.ReadLine();
+    
     }
   }
 
