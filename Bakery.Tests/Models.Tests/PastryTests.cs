893ed3e100afb147b6d88c9444185c96e02783ac
Constantine <faustlarsen@gmail.com>
2020-12-11 13:53:11 -0700

    pass pastry  constructor test

--- Bakery.Tests/Models.Tests/PastryTests.cs
+++ Bakery.Tests/Models.Tests/PastryTests.cs
@@ -6,6 +6,18 @@ namespace Bakery.Tests
   [TestClass]
   public class TestPastry
   {
- 
+    [TestMethod]
+    public void PastryConstructor_InstanceOfPastry_True()
+    {
+    Pastry purchase = new Pastry(2);
+    Assert.AreEqual(typeof(Pastry), purchase.GetType());
+    }
+
+    [TestMethod]
+    public void PastryConstructor_AcceptsAmount_Int()
+    {
+    Pastry purchase = new Pastry(2);
+    Assert.AreEqual(true, purchase.AmountOfPastry());
+    }
   }
 }
